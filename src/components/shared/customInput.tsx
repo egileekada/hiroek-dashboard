864c220a0769f33a916f26b999ff0a9421934d3d
ekadaegile <egileoniso.ekada@gmail.com>
2024-12-09 13:09:21 +0100

    update on input height

--- src/components/shared/customInput.tsx
+++ src/components/shared/customInput.tsx
@@ -54,7 +54,7 @@ export default function CustomInput({ isPassword = false, name, textarea, type, 
                     )}
                     {!textarea &&
 
-                        <div className=" w-full h-[39px] relative " >
+                        <div className=" w-full h-[54px] relative " >
                             <input
                                 {...register(name, {
                                     required: true,
@@ -62,20 +62,20 @@ export default function CustomInput({ isPassword = false, name, textarea, type, 
                                 })}
                                 type={showText} style={{ borderRadius: borderRadius ?? "5px" }} placeholder={placeholder} disabled={disable} value={type === "date" ? new Date(value).toISOString().split('T')[0] : value} name={name} className={` ${hasLeftIcon ? " pl-[40px] " : " "} h-[54px] px-3 border-[#37137F] border-opacity-30 border-[1.5px] outline-none hover:border-[#37137F80] active:border-[#37137F80] focus:border-[#37137F80] bg-transparent w-full text-sm font-medium text-primary `} />
                             {(isPassword) && (
-                                <div role="button" onClick={clickHandler} className={` ${showText === "password" ? "" : "opacity-20"} w-[30px] pr-2 h-[39px] flex justify-center items-center absolute right-0 top-0 `} >
+                                <div role="button" onClick={clickHandler} className={` ${showText === "password" ? "" : "opacity-20"} w-[30px] pr-2 h-[54px] flex justify-center items-center absolute right-0 top-0 `} >
                                     <EyeIcon />
                                 </div>
                             )}
                             {(!isPassword && hasIcon) && (
                                 <div onClick={onclick} role="button" className=" absolute w-fit cursor-pointer right-0 top-0 pr-2 " >
-                                    <div className=" w-[30px] h-[39px] flex justify-center items-center " >
+                                    <div className=" w-[30px] h-[54px] flex justify-center items-center " >
                                         {icon}
                                     </div>
                                 </div>
                             )}
                             {(!isPassword && hasLeftIcon) && (
                                 <div onClick={onclick} role="button" className=" absolute w-fit cursor-pointer left-0 top-0 pr-2 " >
-                                    <div className=" w-[30px] h-[39px] flex justify-center items-center " >
+                                    <div className=" w-[30px] h-[54px] flex justify-center items-center " >
                                         {icon}
                                     </div>
                                 </div>
@@ -94,18 +94,18 @@ export default function CustomInput({ isPassword = false, name, textarea, type, 
                     )}
                     {!textarea &&
 
-                        <div className=" w-full h-[39px] relative " >
+                        <div className=" w-full h-[54px] relative " >
                             <input
                                 onChange={(e) => changeHandler(e.target?.value)}
-                                type={showText} style={{ borderRadius: borderRadius ?? "5px" }} placeholder={placeholder} disabled={disable} value={defaultValue} name={name} className=" h-[39px] px-3 border-[#37137F] border-opacity-30 border-[1.5px] outline-none hover:border-[#37137F80] active:border-[#37137F80] focus:border-[#37137F80] bg-transparent w-full text-sm font-medium text-primary " />
+                                type={showText} style={{ borderRadius: borderRadius ?? "5px" }} placeholder={placeholder} disabled={disable} value={defaultValue} name={name} className=" h-[54px] px-3 border-[#37137F] border-opacity-30 border-[1.5px] outline-none hover:border-[#37137F80] active:border-[#37137F80] focus:border-[#37137F80] bg-transparent w-full text-sm font-medium text-primary " />
                             {(isPassword) && (
-                                <div role="button" onClick={clickHandler} className=" w-[30px] pr-2 h-[39px] flex justify-center items-center absolute right-0 top-0 " >
+                                <div role="button" onClick={clickHandler} className=" w-[30px] pr-2 h-[54px] flex justify-center items-center absolute right-0 top-0 " >
                                     <EyeIcon />
                                 </div>
                             )}
                             {(!isPassword && hasIcon) && (
                                 <div onClick={onclick} role="button" className=" absolute w-fit cursor-pointer right-0 top-0 pr-2 " >
-                                    <div className=" w-[30px] h-[39px] flex justify-center items-center " >
+                                    <div className=" w-[30px] h-[54px] flex justify-center items-center " >
                                         {icon}
                                     </div>
                                 </div>
