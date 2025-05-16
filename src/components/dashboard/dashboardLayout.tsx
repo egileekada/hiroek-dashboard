36dd3e40b4c31850cb2a994501679fabdadad8d2
egileekada <egileoniso.ekada@gmail.com>
2025-05-16 07:42:05 +0100

    update

--- src/components/dashboard/dashboardLayout.tsx
+++ src/components/dashboard/dashboardLayout.tsx
@@ -33,9 +33,6 @@ export default function DashboardLayout() {
                 router("/login")
             }
         }
-
-        hideChat()
-
     }, [setAll, isLoading, setLoading, loading, isError])
 
     return (
