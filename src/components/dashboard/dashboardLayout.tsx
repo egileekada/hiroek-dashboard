796d6a6b17f959a4e7a1000594b7d279f01854ec
egileekada <egileoniso.ekada@gmail.com>
2025-05-21 15:51:11 +0100

    update

--- src/components/dashboard/dashboardLayout.tsx
+++ src/components/dashboard/dashboardLayout.tsx
@@ -5,28 +5,31 @@ import { menulistmobile } from "../../constant"
 import { MobileCashIcon, MobileCommunityIcon, MobileEventIcon, MobileHomeIcon, MobileImpactIcon } from "../../svg"
 import { Text } from "@radix-ui/themes"
 import { useDetails } from "../../global-state/useUserDetails"
-import { useEffect, useState } from "react"
+import { useEffect, useRef, useState } from "react"
 import useUser from "../../hooks/useUser"
-import useTawkTo from "../../services/useTawk"
-// import LoadingAnimation from "../shared/loadingAnimation"
 
 export default function DashboardLayout() {
 
-    const { hideChat } = useTawkTo("680611b92a762a1910951e8f", "1ipbrafch");  
     const router = useNavigate()
     const history = useLocation()
     const { setAll } = useDetails((state) => state);
     const [loading, setLoading] = useState(true)
 
-    useEffect(()=> { 
-        hideChat() 
-    }, [history, router])
 
     const { isLoading, data, isError } = useUser()
 
+    useEffect(() => { 
+        if((window as any).Tawk_API) {
+            (window as any).Tawk_API.minimize();
+            (window as any).Tawk_API.hideWidget();
+        }
+        delete (window as any).Tawk_API;
+        delete (window as any).Tawk_LoadStart;
+    }, [history, router])
+
     useEffect(() => {
         if (!isLoading) {
-            if (data?.email) { 
+            if (data?.email) {
                 setAll({ ...data })
                 setLoading(false)
             } else if (isError) {
@@ -36,7 +39,7 @@ export default function DashboardLayout() {
     }, [setAll, isLoading, setLoading, loading, isError])
 
     return (
-        <> 
+        <>
             <div className=" w-screen h-full relative lg:flex hidden overflow-hidden " >
                 <div className=" w-fit md:block hidden " >
                     <div className=" md:w-[60px] lg:w-[300px] h-screen border-r border-r-[#F0F1F3] " style={{ boxShadow: "4px 0px 30px 0px #8362EA0D" }} >
@@ -50,7 +53,7 @@ export default function DashboardLayout() {
                     <div className={` overflow-y-auto inset-0 absolute flex h-auto lg:pb-0 pb-32
                         ${(history?.pathname?.includes("/dashboard/event/create") || history?.pathname?.includes("/dashboard/donation/withdraw") || history?.pathname?.includes("scan") || history?.pathname?.includes("event/details/bymembers") || history?.pathname?.includes("/dashboard/settings") || history?.pathname?.includes("/dashboard/support") || history?.pathname?.includes("/dashboard/community/details") || history?.pathname?.includes("/dashboard/community/post-comment")) ? "" : " botton-[80px] "}
                         ${(history?.pathname?.includes("/dashboard/event/details") || history?.pathname?.includes("/dashboard/community/details") || history?.pathname?.includes("scan") || history?.pathname?.includes("support") || history?.pathname?.includes("/dashboard/settings") || history?.pathname?.includes("/dashboard/support") || history?.pathname?.includes("/dashboard/community/post-comment")) ? " lg:top-[55px] lg:p-6 top-0 " : " top-[55px] lg:p-6 p-0 "} `} >
-                            <Outlet /> 
+                        <Outlet />
                     </div>
 
                 </div>
